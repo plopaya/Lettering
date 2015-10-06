e0ed929eb72537c3eb6c865e0bd827d8c52e08ec
plopaya <plopaya@engr-134-04.eastern.ewu.edu>
2015-10-06 14:35:38 -0700

    lettering

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Default show No Name
+// Get first name and store
+//var firstName = prompt();
+//console.log(firstName);
+// Get last name and store
+//var lastName = prompt();
+//console.log(lastName);
+
+
+// Display fist and last name
+//$('.first').text(firstName);
+//$('.last').text(lastName);
+
+// turn lettering 
+$('h1').lettering();
